c9d8e8a55a2948eb409e8837bdc242ac9dd38ee2
KHALIL555K <khalil.zeggani.51@gmail.com>
2025-11-10 11:04:27 +0100

    specialite

--- src/js/specialite.js
+++ src/js/specialite.js
@@ -1,2 +1,69 @@
-const specialite = document.getElementById('specialite') ; 
+const specialiteInput = document.getElementById('specialite');
+const btnAjouter = document.getElementById('btnAjouter');
+const listSpecialite = document.getElementById('listSpecialite')
+
+
+let specialites = JSON.parse(localStorage.getItem('specialites')) || [];
+
+const afficherSpecialites = () => {
+
+    specialites.forEach((spec, id) => {
+        const div = document.createElement('div');
+        div.className = 'specialite-item';
+        div.innerHTML = `
+        <span>${spec}</span>
+        <div>
+            <button onclick="modifierSpecialite(${id})">Modifier</button>
+            <button onclick="supprimerSpecialite(${id})">Supprimer</button>
+          </div>`
+        listSpecialite.appendChild(div);
+        console.log(id)
+    })
+}
+
+// formulaire pour ajouter specialite
+const formAjouter = (event) => {
+    event.preventDefault();
+    const valeur = specialiteInput.value.trim();
+    if (valeur) {
+        specialites.push(valeur);
+        localStorage.setItem('specialites', JSON.stringify(specialites))
+        specialiteInput.value = '';
+        afficherSpecialites();
+    }
+}
+
+// supprimer specialite
+const supprimerSpecialite = (id) => {
+    if (confirm('voulez-vous supprimer cette specialite?')) {
+        specialites.splice(id, 1);
+        localStorage.setItem('specialites', JSON.stringify(specialites));
+        afficherSpecialites();
+    }
+}
+
+const modifierSpecialite = (id) => {
+    specialiteInput.value = specialites[id];
+
+    btnAjouter.textContent = 'Modifier';
+
+
+    btnAjouter.onclick = function () {
+        const nouvelleValeur = specialiteInput.value.trim() ; 
+        if(nouvelleValeur){
+            specialites[id] = nouvelleValeur ; 
+            localStorage.setItem('specialites',JSON.stringify(specialites))
+            afficherSpecialites();
+
+            specialiteInput.value = ''; 
+            btnAjouter.textContent = 'Ajouter' 
+            btnAjouter.onclick = formAjouter ; 
+        }
+    }
+}
+
+
+
+
+afficherSpecialites();
 
